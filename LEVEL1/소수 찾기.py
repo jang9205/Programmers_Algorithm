7d175afa9932071fdf6e13c60d4baa9bd13951e1
장동건 <wer4886@naver.com>
2023-06-27 15:16:08 +0900

    레벨 1 수정

--- LEVEL1/소수 찾기.py
+++ LEVEL1/소수 찾기.py
@@ -1,3 +1,4 @@
+# 방법 1
 import math
 
 def solution(n):
@@ -9,3 +10,12 @@ def solution(n):
         else:
             answer += 1
     return answer
+
+# 방법 2(에라토스테네스의 체)
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
